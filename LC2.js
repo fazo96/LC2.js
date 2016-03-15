9608f3bc126e45bb70251af7ace0a7c266956f76
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2016-03-15 20:13:40 +0100

    istruction set 90% done

--- LC2.js
+++ LC2.js
@@ -30,10 +30,17 @@ LC2.index6 = function (data) {
   return data & 63
 }
 
+LC2.trapvect8 = function (data) {
+  return data & 255
+}
+
 LC2.prototype.execute = function (op) {
   var opcode = op >>> 12
-  if (opcode === 0) { // NOP
-    this.nop()
+  if (op === 0) this.nop()
+  else if (opcode === 0) { // BR
+    var nzp = op >>> 9
+    if (nzp === 0) this.nop()
+    else this.br(nzp, this.pgoffset9(op))
   } else if (opcode === 1) { // ADD
     if ((op & 32) > 0) { // use register
       let second_reg = (op & 7)
@@ -46,6 +53,8 @@ LC2.prototype.execute = function (op) {
     this.ld(LC2.dest(op), LC2.pgoffset9(op))
   } else if (opcode === 3) { // ST STORE
     this.st(LC2.dest(op), LC2.pgoffset9(op))
+  } else if (opcode === 4) { // JSR
+    this.jsr(LC2.dest(op), LC2.pgoffset9(op))
   } else if (opcode === 5) { // AND
     if ((op & 64) > 0) { // use register
       let second_reg = (op & 7)
@@ -54,11 +63,27 @@ LC2.prototype.execute = function (op) {
       let num = op & 31
       this.and(LC2.dest(op), LC2.src(op), num, false)
     }
-  } else if (opcode === 9) { // NOT
+  } else if (opcode === 6) { // LDR
+    this.ldr(LC2.dest(op), LC2.src(op), LC2.index6(op))
+  } else if (opcode === 7) { // STR
+    this.str(LC2.dest(op), LC2.src(op), LC2.index6(op))
+  } else if (opcode === 8) { // RTI
+    this.rti()
+  } if (opcode === 9) { // NOT
     if ((op & 63) !== 63) console.log('trailing bits in NOT not set to one (this error is harmless)')
     this.not(LC2.dest(op), LC2.src(op))
+  } else if (opcode === 10) { // LDI
+    this.ldi(LC2.dest(op), LC2.pgoffset9(op))
+  } else if (opcode === 11) { // STI
+    this.sti(LC2.dest(op), LC2.pgoffset9(op))
+  } else if (opcode === 12) { // JSRR
+    this.jsrr(LC2.src(op), LC2.index6(op))
+  } else if (opcode === 13) { // RET
+    this.ret()
   } else if (opcode === 14) { // LEA
     this.lea(LC2.dest(op), LC2.pgoffset9(op))
+  } else if (opcode === 15) { // TRAP
+    this.trap(LC2.trapvect8(op))
   }
 }
 
@@ -75,6 +100,12 @@ LC2.prototype.PC7msb = function () {
 
 // INSTRUCTIONS
 
+LC2.instructions = ['br', 'add', 'ld', 'st', 'jsr', 'and', 'ldr', 'str', 'rti', 'not', 'ldi', 'sti', 'jsrr', 'ret', 'lea', 'trap']
+
+LC2.prototype.br = function (nzp, dest) {
+  if ((this.cc & nzp) !== 0) this.pc = this.PC7msb() + dest
+  else this.pc++
+}
 LC2.prototype.nop = function () { this.pc++ }
 LC2.prototype.add = function (dest, a, b, use_register) {
   this.gpr[dest] = this.gpr[a] + (use_register ? this.gpr[b] : b)
@@ -83,21 +114,30 @@ LC2.prototype.add = function (dest, a, b, use_register) {
 }
 LC2.prototype.ld = function (dest, src) {
   this.gpr[dest] = this.memory[src + this.PC7msb()]
+  this.setCC(dest)
+  this.pc++
 }
 LC2.prototype.st = function (src, data) {
   this.memory[data + this.PC7msb()] = this.gpr[src]
+  this.pc++
 }
 LC2.prototype.ldi = function (dest, src) {
   this.gpr[dest] = this.memory[this.memory[src + this.PC7msb()]]
+  this.setCC(dest)
+  this.pc++
 }
 LC2.prototype.sri = function (src, data) {
   this.memory[this.memory[data + this.PC7msb()]] = this.gpr[src]
+  this.pc++
 }
 LC2.prototype.ldr = function (dest, br, index6) {
   this.gpr[dest] = this.memory[this.gpr[br] + index6]
+  this.setCC(dest)
+  this.pc++
 }
 LC2.prototype.str = function (dest, br, index6) {
   this.memory[this.gpr[br] + index6] = this.gpr[dest]
+  this.pc++
 }
 LC2.prototype.and = function (dest, a, b, use_register) {
   this.gpr[dest] = this.gpr[a] & (use_register ? this.gpr[b] : b)
@@ -110,17 +150,17 @@ LC2.prototype.not = function (dest, src) {
   this.pc++
 }
 LC2.prototype.lea = function (dest, data) {
-  this.gpr[dest] = this.PC7msb() + LC2.pgoffset9(data)
+  this.gpr[dest] = this.PC7msb() + data
   this.setCC(dest)
   this.pc++
 }
-LC2.prototype.jsr = function (trapvect8) {
+LC2.prototype.jsr = function (dest) {
   this.gpr[7] = this.pc
-  this.pc = trapvect8
+  this.pc = this.PC7msb() + dest
 }
 LC2.prototype.trap = function (trapvect8) {
   this.gpr[7] = this.pc
-  this.pc = trapvect8
+  this.pc = this.memory[trapvect8]
 }
 
 // END INSTRUCTIONS
