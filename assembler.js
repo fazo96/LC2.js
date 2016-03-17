ad14d7929b5fe0397ccbd9d7ac465baab808a465
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2016-03-17 19:48:48 +0100

    started testing assembler

--- assembler.js
+++ assembler.js
@@ -43,10 +43,10 @@ Assembler.prototype.assemble = function (str) {
   // TODO: build symbol table now
 
   // --- Processing: convert to object form
-  .map(this.parseInstruction)
+  .map(this.parseInstruction, this)
 
   // --- Postprocessing: convert to binary form (TODO)
-  .map(this.buildInstruction)
+  .map(this.buildInstruction, this)
 }
 
 Assembler.opnames = ['br', 'add', 'ld', 'st', 'jsr', 'and', 'ldr', 'str', 'rti', 'not', 'ldi', 'sti', 'jsrr', 'ret', 'lea', 'trap']
@@ -57,12 +57,12 @@ Assembler.prototype.parseInstruction = function (obj) {
   // Instruction format is "label: opcode arg1[, arg2[, arg3]]"
   if (x.indexOf(':') > 0) { // label is present
     var label = x.split(':', 1)[0]
+    x = x.substring(label.length + 1).trim()
   }
-  x = x.substring(label.length).trim()
-  var parts = x.split(',')
-  var opname = parts[0].trim()
-  var opcode = opname.indexOf('br') === 0 ? 0 : Assembler.opnames.indexOf(opname)
-  var args = parts.slice(1).map(l => l.trim()).map(this.parseArgument)
+  var opname = x.split(' ')[0].toUpperCase()
+  var parts = x.substring(opname.length).split(',')
+  var opcode = opname.indexOf('br') === 0 ? 0 : Assembler.opnames.indexOf(opname.toLowerCase())
+  var args = parts.map(l => l.trim()).map(this.parseArgument, this)
   return { label: label, opname: opname, opcode: opcode, args: args }
 }
 
@@ -81,6 +81,7 @@ Assembler.trapvect8 = function (data) {
 // Takes instruction in object form and returns instruction in binary
 Assembler.prototype.buildInstruction = function (x) {
   var res = x.opcode << 13
+  var dst, src, i6, mem
   if (x.opcode === 0) { // BR
     var nzp = 0
     if (x.opname.indexOf('n') > 0) nzp = nzp | 4
@@ -91,36 +92,36 @@ Assembler.prototype.buildInstruction = function (x) {
   } else if (x.opcode === 1) { // ADD
 
   } else if (x.opcode >= 2 && x.opcode <= 4) { // LD, ST, JSR
-    var reg = this.buildArgument(x.args[0])
-    var frm = Assembler.pgoffset9(this.buildArgument(x.args[1]))
-    return res | frm | (reg << 9)
+    dst = this.buildArgument(x.args[0])
+    src = Assembler.pgoffset9(this.buildArgument(x.args[1]))
+    return res | (dst << 10) | src
   } else if (x.opcode === 5) { // AND
 
   } else if (x.opcode === 6 || x.opcode === 7) { // LDR, STR
-    var dst = this.buildArgument(x.args[0])
-    var src = this.buildArgument(x.args[1])
-    var i6 = Assembler.index6(this.buildArgument(x.args[2]))
-    return res | i6 | (src << 6) | (dst << 9)
+    dst = this.buildArgument(x.args[0])
+    src = this.buildArgument(x.args[1])
+    i6 = Assembler.index6(this.buildArgument(x.args[2]))
+    return res | i6 | (src << 7) | (dst << 10)
   } else if (x.opcode === 8) { // RTI
     return res
   } else if (x.opcode === 9) { // NOT
-    var dst = this.buildArgument(x.args[0])
-    var src = this.buildArgument(x.args[1])
-    return res | 127 | (src << 6) | (dst << 9)
+    console.log(dst, src)
+    console.log((src << 7), (dst << 10))
+    return res | 127 | (src << 7) | (dst << 10)
   } else if (x.opcode === 10 || x.opcode === 11) { // LDI, STI
-    var reg = this.buildArgument(x.args[0])
-    var mem = this.buildArgument(x.args[1])
-    return res | (reg << 9) | mem
+    dst = this.buildArgument(x.args[0])
+    mem = this.buildArgument(x.args[1])
+    return res | (dst << 10) | mem
   } else if (x.opcode === 12) { // JSRR
-    var src = this.buildArgument(x.args[0])
-    var i6 = this.buildArgument(x.args[1])
-    return res | (src << 6) | i6
+    src = this.buildArgument(x.args[0])
+    i6 = this.buildArgument(x.args[1])
+    return res | (src << 7) | i6
   } else if (x.opcode === 13) { // RET
     return res
   } else if (x.opcode === 14) { // LEA
     var pg9 = Assembler.pgoffset9(this.buildArgument(x.args[1]))
-    var dst = this.buildArgument(x.args[0])
-    return res | (dst << 9) | pg9
+    dst = this.buildArgument(x.args[0])
+    return res | (dst << 10) | pg9
   } else if (x.opcode === 15) { // TRAP
     var tv8 = this.buildArgument(x.args[0])
     return res | Assembler.trapvect8(tv8)
@@ -128,7 +129,7 @@ Assembler.prototype.buildInstruction = function (x) {
 }
 
 Assembler.prototype.buildArgument = function (x) {
-
+  return x.literal || x.register || x.variable || 0
 }
 
 // Takes argument in string form and returns argument in object form
@@ -140,3 +141,5 @@ Assembler.prototype.parseArgument = function (arg) {
   else if (/^r[0-7]$/i.test(arg)) return { register: parseInt(arg.slice(1), 10) }
   else return { variable: this.symbolTable[arg] }
 }
+
+if (module && module.exports) module.exports = Assembler
