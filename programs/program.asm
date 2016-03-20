bbe0f0c7db03ba02d9c8061dc51b0a7ee42a4e2c
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2016-03-20 21:29:39 +0100

    improved assembler

--- programs/program.asm
+++ programs/program.asm
@@ -1,11 +1,6 @@
-; This is an example program used to test the LC2.js emulator and assembler
-; Use the included cli.js CLI or globally install this package and use the lc2
-; executable to assemble and run this program
-
-; This program takes a number and writes it back doubled.
-; In the future more serious examples will be bundled with the emulator
-
-trap  x23           ; Read Character (with prompt) into r0
-add   r0, r0, r0    ; double the number
-trap  x21           ; print result
-trap  x25           ; halt
+.ORIG x0
+TRAP x23            ; Read Character (with prompt) into r0
+ADD   r0, r0, r0    ; double the number
+TRAP  x21           ; print result
+TRAP  x25           ; halt
+.END
